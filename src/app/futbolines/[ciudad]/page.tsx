f7599cd6a8723bb94cee543a7236356517dc87c1
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-02-27 21:58:06 +0100

    add cache to futbolines/ciudad

--- src/app/futbolines/[ciudad]/page.tsx
+++ src/app/futbolines/[ciudad]/page.tsx
@@ -2,6 +2,8 @@ import ListaFutbolines from "@/features/FutbolinesCiudad/ListaFutbolines";
 import { getFutbolinesByPlaceId } from "@/shared/services/Places/getLugaresCiudad";
 import { cookies } from "next/headers";
 
+export const revalidate = 3600; // revalida cada 1 hora
+
 const page = async () => {
   const cookiesStore = await cookies();
   const placeId = cookiesStore.get("selectedPlaceId")?.value;
