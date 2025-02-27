9e6547920d7db97391d9a6a22b7aef281184c4a1
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-02-27 21:57:52 +0100

    use google maps service

--- src/app/api/(google-maps)/bares-autocomplete/route.ts
+++ src/app/api/(google-maps)/bares-autocomplete/route.ts
@@ -1,19 +1,11 @@
 import { successResponse } from '@/shared/lib/httpResponse';
+import { GoogleMapsService } from '@/shared/services/GoogleMaps/GoogleMapsService';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const input = searchParams.get("input") || "";
 
-  const key = process.env.NEXT_PUBLIC_MAPS_API_KEY!;
-
-  const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json` +
-    `?input=${encodeURIComponent(input)}` +
-    `&key=${key}` +
-    `&types=establishment` +
-    `&components=country:es`;
-  
-  const res = await fetch(url);
-  const data = await res.json();
+  const data = await GoogleMapsService.autocomplete(input, 'establishment');
   
   return successResponse(data);
 }
