8fa0f49539231a1a2fe552600bd77dd85809863e
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-02-27 22:24:06 +0100

    remove cache from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import "../globals.css";
 
-export const fetchCache = "force-no-store";
-
 export const metadata: Metadata = {
   title: "Futbol-In",
   description: "Encuentra futbolines en tu ciudad!",
