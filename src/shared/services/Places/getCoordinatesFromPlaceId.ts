7051d68507daf924ae2dc590b95000c63f8fdda9
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-02-27 22:25:38 +0100

    add cache to getCoordinatesFromPlaceId

--- src/shared/services/Places/getCoordinatesFromPlaceId.ts
+++ src/shared/services/Places/getCoordinatesFromPlaceId.ts
@@ -1,5 +1,7 @@
-export async function getCoordinatesFromPlaceId(placeId: string) {
-    const res = await fetch(`/api/place-details?placeId=${placeId}`);
-    const data = await res.json();
-    return data;
-  }
+import { cache } from "react";
+
+export const getCoordinatesFromPlaceId = cache(async (placeId: string) => {
+  const res = await fetch(`/api/place-details?placeId=${placeId}`);
+  const data = await res.json();
+  return data;
+});
