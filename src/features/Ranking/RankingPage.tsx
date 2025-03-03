da9f7d2f14b10cf4baf7beb2aa60b10460a136f8
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-03-03 20:26:08 +0100

    ranking page use UserService.getAll

--- src/features/Ranking/RankingPage.tsx
+++ src/features/Ranking/RankingPage.tsx
@@ -1,27 +1,16 @@
-import { UserStatus } from "@/shared/enum/User/Status";
-import connectDb from "@/shared/lib/db";
-import { User } from "@/shared/models/User/User.model";
-import { UserDTO } from "@/shared/models/User/UserDTO";
-import TarjetaUsuarioRanking from "./TarjetaUsuarioRanking";
+import { UserService } from "@/shared/services/User/UserService";
 import Link from "next/link";
+import TarjetaUsuarioRanking from "./TarjetaUsuarioRanking";
 
 export default async function RankingPage() {
-  await connectDb();
 
-  const users = await User.find({}).lean();
-
-  const serializedUsers = (users as unknown as Array<UserDTO & { _id: string }>)
-    .map((user) => ({
-      ...user,
-      id: user._id?.toString(),
-    }))
-    .filter((user) => user.status === UserStatus.DONE);
+  const users = await UserService.getAll();
 
   return (
     <div className="w-full">
       <Link href="/logros" className="text-primary underline underline-offset-2">Logros disponibles</Link>
       <ul className="w-full space-y-2 mt-2">
-        {serializedUsers.map((user) => (
+        {users.map((user) => (
           <TarjetaUsuarioRanking user={user} key={user.id} />
         ))}
       </ul>
