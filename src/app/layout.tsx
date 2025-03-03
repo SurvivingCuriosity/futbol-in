d60a6f2274c05ac182310c355bcb111b51c9592d
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-03-03 21:12:37 +0100

    add session wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,27 @@
+import SessionWrapper from "@/shared/components/SessionWrapper";
 import type { Metadata } from "next";
 import "../globals.css";
 
 export const metadata: Metadata = {
   title: "Futbol-In",
-  description: "Encuentra futbolines en tu ciudad!",
+  description: "Encuentra futbolines en tu ciudad !",
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  
   return (
-    <html lang="es">
-      <head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#0a0a0a" />
-        <link rel="icon" href="/futbolin-logo.svg" />
-      </head>
-      <body className={`antialiased bg-neutral-950 text-neutral-50`}>
-        {children}
-      </body>
-    </html>
+    <SessionWrapper>
+      <html lang="es">
+        <head>
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#0a0a0a" />
+          <link rel="icon" href="/futbolin-logo.svg" />
+        </head>
+        <body className={`antialiased bg-neutral-950 text-neutral-50`}>
+          {children}
+        </body>
+      </html>
+    </SessionWrapper>
   );
 }
--- src/app/logros/ranking/layout.tsx
+++ src/app/logros/ranking/layout.tsx
@@ -1,10 +1,8 @@
 import { NavLayout } from "@/shared/components/Layouts/NavLayout";
-import { getServerSession } from "next-auth";
 import React from "react";
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
-  const session = await getServerSession();
-  return <NavLayout loggedIn={!!session}>{children}</NavLayout>;
+  return <NavLayout loggedIn={true}>{children}</NavLayout>;
 };
 
 export default layout;
