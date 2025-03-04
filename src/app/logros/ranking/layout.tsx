fff8330a5fde94b18a59eeb2e9a27713a98645cf
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-03-04 21:46:40 +0100

    Delete layout.tsx

--- src/app/logros/ranking/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { NavLayout } from "@/shared/components/Layouts/NavLayout";
-import React from "react";
-
-const layout = async ({ children }: { children: React.ReactNode }) => {
-  return <NavLayout loggedIn={true}>{children}</NavLayout>;
-};
-
-export default layout;
