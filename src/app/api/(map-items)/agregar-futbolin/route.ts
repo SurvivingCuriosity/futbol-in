f728771ae3906bcfd95c05e8ddfffd40af9d3730
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-03-07 20:41:04 +0100

    Update agregar futbolin route

--- src/app/api/(map-items)/agregar-futbolin/route.ts
+++ src/app/api/(map-items)/agregar-futbolin/route.ts
@@ -1,15 +1,31 @@
+import { authOptions } from "@/shared/lib/authOptions";
 import connectDb from "@/shared/lib/db";
 import { errorResponse, successResponse } from "@/shared/lib/httpResponse";
-import Lugar from '@/shared/models/Lugar/Lugar.model';
+import Lugar from "@/shared/models/Lugar/Lugar.model";
+import { LugarDTO } from "@/shared/models/Lugar/LugarDTO";
+import { UserService } from "@/shared/services/User/UserService";
+import { getServerSession } from "next-auth";
 
 export async function POST(req: Request) {
   try {
     await connectDb();
-    const { nombre, direccion, lat, lng, googlePlaceId, tipoFutbolin, tipoLugar } = await req.json();
+    const lugar: LugarDTO = await req.json();
+    const {
+      nombre,
+      direccion,
+      coordinates: [lng, lat],
+      googlePlaceId,
+      tipoLugar,
+      tipoFutbolin,
+      verificado,
+    } = lugar;
 
     if (!nombre || !direccion || !lat || !lng || !googlePlaceId) {
-      return successResponse('Falta algún campo', 400);
+      return successResponse("Falta algún campo", 400);
     }
+
+    const session = await getServerSession(authOptions);
+    const user = session?.user;
 
     const newLugar = await Lugar.create({
       nombre,
@@ -21,7 +37,12 @@ export async function POST(req: Request) {
       },
       tipoLugar,
       tipoFutbolin,
+      verificado,
     });
+
+    if (user) {
+      await UserService.incrementUserStat(user.id, "addedFutbolines");
+    }
 
     return successResponse({ success: true, futbolin: newLugar }, 201);
   } catch (error) {
