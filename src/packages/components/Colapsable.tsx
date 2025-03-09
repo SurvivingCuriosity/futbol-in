e4e0e785b6c5f1b5ed337176a7b5d62025966821
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-03-09 05:24:23 +0100

    Create Colapsable.tsx

---
+++ src/packages/components/Colapsable.tsx
@@ -0,0 +1,25 @@
+export interface ColapsableProps {
+  open: boolean;
+  containerClassName?: string;
+  visibleContent?: React.ReactNode;
+  extraContent?: React.ReactNode;
+}
+export const Colapsable = (props: ColapsableProps) => {
+  const { open, visibleContent, extraContent, containerClassName } = props;
+  return (
+    <div className={containerClassName}>
+      {visibleContent}
+      <div
+        style={{
+          gridTemplateRows: open ? "1fr" : "0fr",
+          transition: "grid-template-rows 0.2s ease-out",
+        }}
+        className="wrapper grid"
+      >
+        <div className="inner overflow-hidden">
+          {extraContent}
+        </div>
+      </div>
+    </div>
+  );
+};
