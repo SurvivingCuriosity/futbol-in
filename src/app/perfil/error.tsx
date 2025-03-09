e8b7cf9a4d872de61e5cd3b3a95e2e415ffb23db
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-03-09 15:35:11 +0100

    signout on perfil error

---
+++ src/app/perfil/error.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { signOut } from "next-auth/react";
+import { useEffect } from "react";
+
+const Error = () => {
+  useEffect(() => {
+    signOut({ redirect: true, callbackUrl: "/" });
+  }, []);
+
+  return <div>error</div>;
+};
+
+export default Error;
