376ba67a4b553476ac399f8b07533ba8aac346e1
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-03-15 18:55:47 +0100

    Update route.ts

--- src/app/api/user/get/route.ts
+++ src/app/api/user/get/route.ts
@@ -1,21 +1,16 @@
-import { UserService } from "@/server/services/User/UserService";
 import { TypedRequest } from "@/core/types/Request/TypedRequest";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/server/lib/authOptions";
 import { errorResponse, successResponse } from "@/server/lib/httpResponse";
+import { UserService } from "@/server/services/User/UserService";
+import { validateLoggedInUser } from "@/server/validations/shared/validateLoggedInUser";
 
 export async function POST(req: TypedRequest<{idUser: string}>) {
   try {
     const { idUser } = await req.json();
 
-    // Obtención del usuario que realiza la petición
-    const session = await getServerSession(authOptions);
-    const user = session?.user;
-    if (!user?.id) {
-      return errorResponse("No autenticado", 401);
-    }
+    await validateLoggedInUser()
 
     const userDb = await UserService.findById(idUser);
+    
     if (!userDb) {
       return errorResponse("Usuario no existe", 404);
     }
