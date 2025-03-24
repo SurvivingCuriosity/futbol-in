1e0e07cbb26b4cb6e6754c75909ec7a3ce9ae19f
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-03-24 12:37:14 +0100

    add presas evo logo

--- src/client/shared/constants/FutbolinesLogoImageMap.ts
+++ src/client/shared/constants/FutbolinesLogoImageMap.ts
@@ -1,5 +1,6 @@
 import logo_infinity from "@/client/shared/assets/img/logos_futbolines/infinity.png";
 import logo_tsunami from "@/client/shared/assets/img/logos_futbolines/tsunami.png";
+import logo_presasevo from "@/client/shared/assets/img/logos_futbolines/presas_evo.png";
 import madera from "@/client/shared/assets/img/logos_futbolines/madera.svg";
 import generico from "@/client/shared/assets/img/logos_futbolines/generico.png";
 
@@ -8,8 +9,8 @@ import { StaticImageData } from "next/image";
 
 export const ImagenFutbolinLogoMap: Record<TipoFutbolin, StaticImageData> = {
   [TipoFutbolin.TSUNAMI]: logo_tsunami,
-  [TipoFutbolin.PRESAS]: logo_tsunami,
-  [TipoFutbolin.PRESAS_EVO]: logo_tsunami,
+  [TipoFutbolin.PRESAS]: logo_presasevo,
+  [TipoFutbolin.PRESAS_EVO]: logo_presasevo,
   [TipoFutbolin.DESCONOCIDO]: generico,
   [TipoFutbolin.MADERA]: madera,
   [TipoFutbolin.INFINITY]: logo_infinity,
