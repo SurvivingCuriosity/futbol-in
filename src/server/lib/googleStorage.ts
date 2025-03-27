252da02dfe82a7b63f595a130f16a0d090c93b09
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-03-27 22:00:45 +0100

    Update googleStorage.ts

--- src/server/lib/googleStorage.ts
+++ src/server/lib/googleStorage.ts
@@ -1,11 +1,21 @@
-import { Storage } from '@google-cloud/storage';
-import path from 'path';
+import { Storage } from "@google-cloud/storage";
 
-const keyPath = path.join(process.cwd(), 'keys', 'futbolinapp-72fdfad32904.json');
+if (!process.env.GCP_CREDENTIALS) {
+  throw new Error("Falta la variable de entorno GCP_CREDENTIALS_BASE64");
+}
 
+// Decodificamos la variable de entorno BASE64
+const decoded = Buffer.from(process.env.GCP_CREDENTIALS, "base64").toString("utf8");
+const credentials = JSON.parse(decoded);
+
+// Creamos el cliente de Google Cloud Storage sin keyFilename
 const storage = new Storage({
-  keyFilename: keyPath,
-  projectId: 'futbolinapp'
+  projectId: credentials.project_id,
+  credentials: {
+    client_email: credentials.client_email,
+    private_key: credentials.private_key,
+  },
 });
 
-export const bucket = storage.bucket('futbolin');
+// Apuntamos al bucket
+export const bucket = storage.bucket("futbolin");
