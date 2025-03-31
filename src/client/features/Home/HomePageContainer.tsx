261ba75d93df5195fedf8f48db6cc04c6d112442
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-03-31 12:05:40 +0200

    Update HomePageContainer.tsx

--- src/client/features/Home/HomePageContainer.tsx
+++ src/client/features/Home/HomePageContainer.tsx
@@ -3,8 +3,14 @@ import HomePage from "./HomePage";
 import { NotificacionesService } from "@/server/services/Notificaciones/NotificacionesService";
 import { UserDTO } from "@/server/models/User/UserDTO";
 import { INotificaciones } from "@/core/types/Notificaciones/INotificaciones";
+import { signOut } from "next-auth/react";
 
 const HomePageContainer = async ({ user }: { user: UserDTO | undefined }) => {
+
+  if(!user){
+    signOut()
+  }
+
   const equiposPendientes = await NotificacionesService.getInvitacionesAEquipos(
     user?.id || ""
   );
