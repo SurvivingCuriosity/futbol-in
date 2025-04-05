c27340afb6b9043d02d9866b2c0718af70e4f606
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-04-05 04:05:09 +0200

    nuevos colores

--- src/client/features/Torneos/components/Nav.tsx
+++ src/client/features/Torneos/components/Nav.tsx
@@ -5,7 +5,7 @@ import {
   faPeopleGroup,
   faUser,
   faUserPlus,
-  IconDefinition
+  IconDefinition,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { InlinePicker } from "futbol-in-ui";
@@ -24,7 +24,7 @@ export const Nav = ({
 
   const [activeTabId, setActiveTabId] = useState(getTabIdFromPath(pathname));
 
-  const detalles = { id: 0, label: "Info", icon: faInfoCircle };
+  const detalles = { id: 0, label: "Información", icon: faInfoCircle };
   const equipos = { id: 1, label: "Equipos", icon: faPeopleGroup };
   const clasificacion = { id: 2, label: "Clasific.", icon: faList12 };
   const partidos = { id: 3, label: "Partidos", icon: faUser };
@@ -82,9 +82,21 @@ export const Nav = ({
 
   return (
     <div className="my-2 w-full md:min-w-md">
-      <InlinePicker options={opciones} onTabClick={handleTabClick} size="sm" activeTabId={activeTabId}/>
-      <h2 className="text-xl font-bold bg-neutral-800 text-neutral-400 mt-2 p-1">
-        <FontAwesomeIcon icon={iconMap[activeTabId]} width={20} height={20} className="mr-2"/>
+      <InlinePicker
+        options={opciones}
+        onTabClick={handleTabClick}
+        size="sm"
+        activeTabId={activeTabId}
+        itemContainerClassName="flex items-center p-3 justify-center md:justify-start gap-2"
+        textClassName="hidden md:block text-sm w-fit xl:mx-0 lg:text-lg xl:text-xl"
+      />
+      <h2 className="text-xl font-bold text-neutral-400 mt-2 p-1">
+        <FontAwesomeIcon
+          icon={iconMap[activeTabId]}
+          width={24}
+          height={24}
+          className="mr-2 text-lg"
+        />
         {headingText[activeTabId]}
       </h2>
     </div>
