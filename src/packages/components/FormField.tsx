4957b3c2594760529d89c99d4682129f72b53b68
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-04-14 19:24:48 +0200

    Update FormField.tsx

--- src/packages/components/FormField.tsx
+++ src/packages/components/FormField.tsx
@@ -1,6 +1,6 @@
-export const FormField = ({children, inline = false}:{children:React.ReactNode | React.ReactNode[], inline?:boolean}) => {
+export const FormField = ({children, inline = false, className = ""}:{children:React.ReactNode | React.ReactNode[], inline?:boolean, className?:string}) => {
     return (
-        <div className={`flex ${inline ? 'space-x-1 items-center' : 'flex-col space-y-1'} mb-4 w-full`}>
+        <div className={`flex ${inline ? 'space-x-1 items-center' : 'flex-col space-y-1'} mb-4 w-full ${className}`}>
             {children}
         </div>
     )
