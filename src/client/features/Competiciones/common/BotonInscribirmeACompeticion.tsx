6169024c945f3fc815e57283864681f5cb63bec0
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-04-14 14:18:24 +0200

    Update BotonInscribirmeACompeticion.tsx

--- src/client/features/Competiciones/common/BotonInscribirmeACompeticion.tsx
+++ src/client/features/Competiciones/common/BotonInscribirmeACompeticion.tsx
@@ -1,10 +1,7 @@
-"use client";
-
 import { EstadoCompeticion } from "@/core/enum/Competicion/EstadoCompeticion";
 import { TipoCompeticion } from "@/core/enum/Competicion/TipoCompeticion";
 import { TipoInscripcion } from "@/core/enum/Competicion/TipoInscripcion";
-import { Button } from "futbol-in-ui";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export const BotonInscribirmeACompeticion = ({
   tipoInscripcion,
@@ -18,31 +15,23 @@ export const BotonInscribirmeACompeticion = ({
   tipoCompeticion: TipoCompeticion
 }) => {
 
-  const router = useRouter()
-
   const mostrarBotonInscribirme =
     tipoInscripcion !== TipoInscripcion.CERRADO &&
     estadoCompeticion === EstadoCompeticion.ACTIVO;
 
-  const handleInscribirme = () => {
-    if(tipoCompeticion === TipoCompeticion.TORNEO) {
-      router.push(`/competitivo/torneos/${idCompeticion}/join`);
-    } 
-    
-    if(tipoCompeticion === TipoCompeticion.LIGA) {
-      router.push(`/competitivo/ligas/${idCompeticion}/join`);
-    }
-  };
 
   return (
     <div className="w-fit">
       {mostrarBotonInscribirme && (
-        <Button
-          label="Inscribirme"
-          onClick={handleInscribirme}
-          variant="primary"
-          size="sm"
-        />
+        
+        <Link href={
+          tipoCompeticion === TipoCompeticion.TORNEO
+            ? `/competitivo/torneos/${idCompeticion}/join`
+            : `/competitivo/ligas/${idCompeticion}/join`
+        }>
+          Inscribirme
+        </Link>
+      
       )}
     </div>
   );
