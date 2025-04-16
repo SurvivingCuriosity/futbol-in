534770a0376856083ff9639a8275c789317294f3
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-04-16 14:04:06 +0200

    Update agregarSpotValidation.ts

--- src/server/validations/spots/agregarSpotValidation.ts
+++ src/server/validations/spots/agregarSpotValidation.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
 
 export const agregarSpotSchema = z.object({
-  nombre: z.string().nonempty("El campo 'nombre' es requerido."),
+  nombre: z.string().nonempty("No has introducido el bar o dirección"),
   direccion: z.string().nonempty("El campo 'direccion' es requerido."),
   coordinates: z
     .array(z.number())
     .length(2, "Se requieren 2 valores en 'coordinates' (lng, lat)."),
   googlePlaceId: z.string().nonempty("El campo 'googlePlaceId' es requerido."),
-  ciudad: z.string(),
+  ciudad: z.string().nonempty("El campo 'ciudad' es requerido."),
   tipoLugar: z.string().nonempty("El campo 'tipoLugar' es requerido."),
   tipoFutbolin: z.string().nonempty("El campo 'tipoFutbolin' es requerido."),
   distribucion: z.string().nonempty("El campo 'distribucion' es requerido."),
