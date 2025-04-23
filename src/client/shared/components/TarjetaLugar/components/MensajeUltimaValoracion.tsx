fb3145feab3c7bafde80d459615841b0ec468ab8
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-04-23 12:32:35 +0200

    Delete MensajeUltimaValoracion.tsx

--- src/client/shared/components/TarjetaLugar/components/MensajeUltimaValoracion.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const MensajeUltimaValoracion = () => {
-  return (
-    <p className="py-1 text-neutral-500 text-sm">
-      Última valoración <span className="text-green-600">positiva</span> ayer
-    </p>
-  );
-};
