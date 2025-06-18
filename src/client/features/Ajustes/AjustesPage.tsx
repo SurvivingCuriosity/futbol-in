f7f1f3ef55dd43209e2e025b01aa50d9e84e20f5
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-06-18 12:53:23 +0200

    Mensaje en obras ajustes page

--- src/client/features/Ajustes/AjustesPage.tsx
+++ src/client/features/Ajustes/AjustesPage.tsx
@@ -4,6 +4,9 @@ import { AjustesPerfil } from "./Perfil/AjustesPerfil";
 
 export const AjustesPage = () => {
   return (
+    <p className="text-center text-sm text-neutral-500 p-10">Ups... aún no hay nada que ajustar</p>
+  )
+  return (
     <div className="space-y-4 mt-4">
       <TextInput placeholder="Buscar..." />
       <AjustesPerfil />
