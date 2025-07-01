e2f0e5da56ff714fc3ae82db621bc6c3f0d4af37
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-07-01 13:33:01 +0200

    cache mapa global

--- src/app/spots/global/page.tsx
+++ src/app/spots/global/page.tsx
@@ -1,6 +1,8 @@
 import { MapaGlobalPage } from "@/client/features/Mapa/MapaGlobalPage";
 import { SpotService } from "@/server/services/Spots/SpotsService";
 
+export const revalidate = 60;
+
 const page = async () => {
   const spots = await SpotService.getAll();
 
